b6d7faf000a3e5b70825b321d79b9ce02b443308
Mateo <mateo.carvajal930@pascualbravo.edu.co>
2022-01-23 10:13:20 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/cursoFS/Fullstack/FullstackApplication.java
@@ -0,0 +1,14 @@
+package com.cursoFS.Fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class FullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cursoFS/Fullstack/dao/UsuarioDao.java
@@ -0,0 +1,16 @@
+package com.cursoFS.Fullstack.dao;
+
+import com.cursoFS.Fullstack.models.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDao {
+
+    List<Usuario> getUsuarios();
+
+    void eliminar(Long id);
+
+    void registrar(Usuario usuario);
+
+    Usuario obtenerUsuarioPorCredenciales(Usuario usuario);
+}
